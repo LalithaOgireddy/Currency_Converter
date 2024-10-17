9928a742b639f2d3bc4615485de02da0677c6ad3
Lalitha Ogireddy <lalitha.ogireddy@gmail.com>
2024-10-17 12:40:16 +0200

    progress commit

--- src/main/java/com/lalitha/CurrencyCalculator.java
+++ src/main/java/com/lalitha/CurrencyCalculator.java
@@ -1,6 +1,10 @@
 package com.lalitha;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class CurrencyCalculator {
     double inputAmt;
@@ -27,25 +31,25 @@ public class CurrencyCalculator {
                 System.out.println("Enter amount in SEK");
                 excRate = ExchangeRates.SEK_to_USD;
                 inputAmt = getInputAmount();
-                exchangeCalculation(excRate,inputAmt);
+                exchangeCalculation(excRate,inputAmt,"SEK","USD");
                 break;
             case 2:
                 System.out.println("Enter amount in USD");
                 excRate = ExchangeRates.USD_to_SEK;
                 inputAmt = getInputAmount();
-                exchangeCalculation(excRate,inputAmt);
+                exchangeCalculation(excRate,inputAmt,"USD","SEK");
                 break;
             case 3:
                 System.out.println("Enter amount in SEK");
                 excRate = ExchangeRates.SEK_to_EUR;
                 inputAmt = getInputAmount();
-                exchangeCalculation(excRate,inputAmt);
+                exchangeCalculation(excRate,inputAmt,"SEK","EUR");
                 break;
             case 4:
                 System.out.println("Enter amount in Euro");
                 excRate = ExchangeRates.EUR_to_SEK;
                 inputAmt = getInputAmount();
-                exchangeCalculation(excRate,inputAmt);
+                exchangeCalculation(excRate,inputAmt,"EUR","SEK");
                 break;
             case 0:
                 break;
@@ -65,16 +69,27 @@ public class CurrencyCalculator {
 
     public boolean validateInput(double inputAmt){
         if(inputAmt < 0.00){
-            System.out.println("negative");
+            //System.out.println("negative");
+            System.out.println("Amount should not be negative");
             return false;
         }
         return true;
     }
 
-    public void exchangeCalculation(double rate, double inputAmt){
+    public void exchangeCalculation(double rate, double inputAmt, String fromCrncy, String toCrncy){
         if(validateInput(inputAmt)){
             outputAmt = inputAmt * rate;
             System.out.println(outputAmt);
-        } else System.out.println("Amount should not be negative");
+            formatOutput(inputAmt,outputAmt,fromCrncy,toCrncy);
+        }
+    }
+
+    public void formatOutput(double inputAmt, double outputAmt, String fromCrncy, String toCrncy){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String dateTime = localDateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss"));
+        DecimalFormat formatter = new DecimalFormat("#,##0.00");
+        BigDecimal outAmt = new BigDecimal(String.valueOf(outputAmt));
+        String outAmtStr = formatter.format(outAmt);
+        System.out.println(fromCrncy+" "+inputAmt +" = "+toCrncy+" "+outAmtStr +" at "+ dateTime);
     }
 }
